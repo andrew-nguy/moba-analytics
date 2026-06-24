b9a680374a0b75c8b958b51195297a8a71e749df
nguay097 <andrew.nguyen132@outlook.com>
2026-06-24 17:02:08 +0930

    Add initial Spring Boot scaffold

---
+++ src/main/java/com/nguay097/moba_analytics/MobaAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.nguay097.moba_analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobaAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobaAnalyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nguay097/moba_analytics/MobaAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nguay097.moba_analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobaAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
